09b8ffc759bdb42c24375bfc5239c98023fe12ec
Lilco <metra.lilco@gmail.com>
2023-10-18 20:51:25 +0300

    Create Teacher model

--- university_project/university/models.py
+++ university_project/university/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=150, verbose_name="First name")
+    last_name = models.CharField(max_length=200, verbose_name="Last name")
+    birth_date = models.DateField(verbose_name="Date of birth")
+    subject = models.CharField(max_length=150, verbose_name="Subject")
