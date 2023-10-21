89309e694d99d86017325869b18f00067f751bf3
Lilco <metra.lilco@gmail.com>
2023-10-21 20:38:08 +0300

    teacher_form.html - code is written

--- university_project/university/views.py
+++ university_project/university/views.py
@@ -6,8 +6,18 @@ from .models import Group
 
 
 def teacher_form(request):
+    if request.method == "GET":
+        return render(request, "teacher_form.html")
+    print("Дані форми отримано!", request.POST)
+    t = Teacher.objects.create(
+        first_name=request.POST["first_name"],
+        patronymic=request.POST["patronymic"],
+        last_name=request.POST["last_name"],
+        birth_date=request.POST["birth_date"],
+        subject=request.POST["subject"],
+    )
+    print("Запис збережено: ", t)
     return render(request, "teacher_form.html")
-
 
 def group_form(request):
     return render(request, "group_form.html")
